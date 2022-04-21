245ae7718a4d887038614c4261e03975aa2c26cd
Kuandyk20 <rahatkuandykov21@gmail.com>
2022-04-21 11:46:31 +0600

    Withdraw page with balance

---
+++ pages/withdraw/index.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="main" :style="mainStyle">
+    <h1>RANT</h1>
+    <h1>Withdraw</h1>
+
+    <p> Balance:{{bal}} </p>
+
+    <input
+      v-model="money"
+      type="number"
+      name="email"
+      id="email"
+      placeholder="Type your sum"
+    />  
+    <button
+      type="button"
+      class="button2"
+      id="withdraw"
+      :style="inputStyle"
+      @click.prevent="withdraw"
+    >
+      Withdraw
+    </button>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+    mainStyle: String,
+    inputStyle: String,
+  },
+  data() {
+    return {
+    bal: '',
+    money: ''
+    }
+  },
+  async mounted(){
+        const user = JSON.parse(localStorage.getItem('user'));
+        const response = await fetch(`https://sf-rant-backend.herokua pp.com/user/${user.id}/balance`, {
+       method: 'GET',
+        headers: {'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+        }
+      })
+      const con = await response.json();
+        console.log(con);
+        this.bal = JSON.parse(con.balance);
+        
+        localStorage.setItem('wallet', JSON.stringify(con));
+
+    },
+    
+       
+    
+    methods: {
+    name: 'withdraw',
+     async withdraw() {
+         
+         const wall = JSON.parse(localStorage.getItem('wallet'));
+         if(wall.balance >= this.money){
+        const response = await fetch(`https://sf-rant-backend.herokuapp.com/user/${user.id}/withdraw`, {
+          method: 'POST',
+          
+          headers:
+              {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+              },
+              
+            body: JSON.stringify({
+                balance: wall.balance - this.money
+            })
+            
+        })
+        console.log(response.json());
+        await this.$router.push('/');
+        } else {
+            alert('No enough money.')
+        }
+        
+        
+     }
+
+        
+
+    }
+  }
+</script>
+
+
+
+
+
+
+
+
+
+
+<style>
+/* Import Poppins font: */
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+.main {
+  background: rgba(255, 255, 255, 0.4);
+  position: absolute;
+  top: 20%;
+  left: 30%;
+  width: 40%;
+  text-align: center;
+  padding: 5px;
+  border-radius: 3rem;
+  box-shadow: 0px 0px 8px -5px #000000;
+  padding-top: 3%;
+  padding-bottom: 5%;
+  font-family: "Poppins", sans-serif;
+}
+h1 {
+  cursor: default;
+  user-select: none;
+}
+
+.fa {
+  font-size: 28px;
+  color: #000000;
+}
+
+input {
+  border-radius: 3rem;
+  border: none;
+  padding: 10px;
+  text-align: center;
+  outline: none;
+  margin: 10px;
+  width: 30%;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+}
+input:hover {
+  box-shadow: 0px 0px 8px -5px #000000;
+}
+input:active {
+  box-shadow: 0px 0px 8px -5px #000000;
+}
+#signin {
+  box-shadow: grey;
+}
+.button {
+  cursor: pointer;
+  user-select: none;
+}
+
+.button2 {
+  cursor: pointer;
+  user-select: none;
+  background: white;
+}
+img {
+  height: 2.2rem;
+  margin: 10px;
+  user-select: none;
+}
+img:hover {
+  box-shadow: 0px 0px 8px -5px #000000;
+  cursor: pointer;
+  border-radius: 200rem;
+}
+</style>
